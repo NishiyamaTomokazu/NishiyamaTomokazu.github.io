f2466c0e536cd35d38c42d4a3ce106e2d9e5870b
NishiyamaTomokazu <60099111+NishiyamaTomokazu@users.noreply.github.com>
2021-10-01 14:27:39 +0900

    変更

--- docs/sendSound.js
+++ docs/sendSound.js
@@ -93,15 +93,13 @@ function sendTime() {
     sendDataArray[1] = 21;
     sendDataArray[2] = 10;
     sendDataArray[3] = 21;
-    sendDataArray[4] = 10
-    sendDataArray[5] = 1
-
-    sendDataArray[6] = 1;
-    sendDataArray[7] = 10;
-    sendDataArray[8] = 7;
-    sendDataArray[9] = 1;
-    sendDataArray[10] = 13;
-    sendDataArray[11] = 58;
+    sendDataArray[4] = 10;
+    sendDataArray[5] = 1;
+    sendDataArray[6] = 10;
+    sendDataArray[7] = 7;
+    sendDataArray[8] = 1;
+    sendDataArray[9] = 13;
+    sendDataArray[10] = 58;
     sendDataBySound(sendDataArray);
     console.log(sendDataArray);
 }
